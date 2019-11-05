2ee82a3328f7ed85cc77313c60f5d9d970a916f1
Antonio Borrero Granell <me@antoniobg.com>
2019-11-05 11:47:48 +0100

    Update package info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,46 @@
 defmodule JsonConfix.MixProject do
   use Mix.Project
 
+  @project_url "https://github.com/quri/json_confix"
+  @version "0.2.0"
+
   def project do
     [
       app: :json_confix,
-      version: "0.2.0",
-      elixir: "~> 1.6",
+      name: "JsonConfix",
+      version: @version,
+      elixir: "~> 1.6 or ~> 1.9",
+      package: package(),
+      description: description(),
       start_permanent: Enum.member?([:prod, :int], Mix.env()),
+      docs: docs(),
+
       deps: deps()
+    ]
+  end
+
+
+  defp package do
+    [
+      maintainers: ["Trax Retail"],
+      licenses: ["Beerware"],
+      links: %{
+        "GitHub" => @project_url,
+        "ConfigTuples" => "https://github.com/rockneurotiko/config_tuples"
+      }
+    ]
+  end
+
+  defp description do
+    "JsonConfix is a Elixir config provider for Distillery that replaces config tuples (e.g `{:json, value}`) \
+    to the correspondent value read from a JSON file."
+  end
+
+  def docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_url: "https://github.com/quri/json_confix"
     ]
   end
 
